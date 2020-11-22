7f9dbfe0b6b1e90ef31dc4abb6be63e6f1b0e89f
Piotr <piotr.gryczan@gmail.com>
2020-11-22 22:32:25 +0100

    Zadanie domowe numer 2

--- src/MainApp.java
+++ src/MainApp.java
@@ -2,18 +2,42 @@ public class MainApp {
 
     public static void main(String[] args) {
 
-        int liczba1 = 100;
-        int liczba2 = 123;
-        int liczba3 = 234;
-        int liczba4 = 142312;
+//        User piotr = new User("Piotr", "Testowy", "asd@asd.pl", 13);
+//        piotr.getFullName();
+//        piotr.getAllData();
+//        System.out.println(piotr.getUserAge());
+//        System.out.println(piotr.isUserAdult());
+//        piotr.greetings(piotr.firstName);
+//        System.out.println(piotr.yourAgePlusTen(piotr.age));
+//        piotr.greetings(piotr.firstName, piotr.lastName);
+//
+//        System.out.println("");
+//
+//        User tomek = new User("Tomek", "Testowy", "asd2@asd.pl", 25);
+//        tomek.getFullName();
+//        tomek.getAllData();
+//        System.out.println(tomek.getUserAge());
+//        System.out.println(tomek.isUserAdult());
+//        tomek.howOldAreYou(tomek.firstName, tomek.age);
+//
+//        System.out.println("");
+//
+//        User filip = new User("Filip", "Testowy", "mail@mail.com", 25);
+//        filip. getAllData();
 
-        boolean suma1 = (liczba1 + liczba2) % 2 == 0;
-        boolean suma2 = (liczba3 + liczba4) % 2 == 0;
 
-        if (suma1 && suma2) {
-            System.out.println("Wszstkie liczby są parzyste");
-        } else {
-            System.out.println("Co najmniej jedna z liczb jest nieparzysta");
-        }
+        Bug bug = new Bug("Długi opis", "asd@asd.pl", 5);
+        bug.showData();
+        bug.showReporterEmail();
+        bug.showStatus();
+        System.out.println(bug.getPriority());
+
+        Bug bug2 = new Bug("Długi opis", "asd@asd.pl", 100);
+        bug2.closeBug();
+        bug2.showStatus();
+        System.out.println(bug2.getPriority());
+
+        Bug bug3 = new Bug("Bardzo długi opis", "email@email.com", -1);
+        System.out.println(bug3.getPriority());
     }
 }
